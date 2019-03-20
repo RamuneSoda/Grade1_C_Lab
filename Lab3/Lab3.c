0dfa4ffc9994e8a218ea80d65de00645b3a3b6ae
Caixiyang <42068092+RamuneSoda@users.noreply.github.com>
2019-03-21 00:17:37 +0800

    190321

---
+++ Lab3/Lab3.c
@@ -0,0 +1,5 @@
+int main(int argc , char* argv[])
+{
+    run (argc , argv); //调用程序主功能实现函数
+    return 0 ;
+}
---
+++ Lab3/Lab3_fun.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define random(x) (rand () % x)
+
+int isNum(char*, int);
+int isPath(char *, int);
+void BuildDataFile(char*);
