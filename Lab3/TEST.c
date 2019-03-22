7f5c4aeb6154628d013fa9fc7c29cfba6829a672
Cai Xiyang <caixiyang2000@gmail.com>
2019-03-22 11:37:42 +0800

    TEST added

---
+++ Lab3/TEST.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int isPath(char *pPath)
+{
+    int len = strlen(pPath);
+    if(strstr(pPath, "\\") - pPath == 0) //不能以\\开头
+    {
+        return 0;
+    }
+    if(pPath[0] >= 'A' && pPath[0] <= 'Z') //是绝对路径吗？
+    {
+        if(strstr(pPath, ":/") == (pPath + 1) || strstr(pPath, ":\\") == (pPath + 1) )
+        {
+            for(int i = 2; i <= len -1; i++)
+            {
+                
+            }
+        }
+        else
+        {
+            return 0;
+        }
+        
+
+        if(strstr(pPath, "\\") == (pPath + len -1) ||(strchr(pPath, '/') == (pPath + len )))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    char path[100];
+    gets(path);
+    printf("%d",isPath(path));
+
+}
