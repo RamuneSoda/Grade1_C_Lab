d7e85771befcdb72b7dc0a613bb19023d5dcf1c0
Cai Xiyang <caixiyang2000@gmail.com>
2019-04-09 20:17:27 +0800

    Lab4_launch

---
+++ Lab4/Lab4.c
@@ -0,0 +1,6 @@
+#include "Lab4_run.h"
+int main(int argc , char* argv[])
+{
+    run (argc , argv); //调用程序主功能实现函数
+    return 0 ;
+}
---
+++ Lab4/Lab4_fun.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <direct.h>
+#include <io.h>
+#include "Lab4_data.h"
+
+#define random(x,y) (rand () % y + x) //x:min y:max
+
+int isNum(char*, int);
+int isPath(char *);
+int isAbsPath(char *);
+int isRelPath(char *);
+int isFileExist(char *);
+void DivPath(char *, CONF*);
+void BuildDataFile(CONF *);
+void mkdir_m(char *);
+int GetOffset(char *);
+int count(char *);
+void iniconf(CONF*);
